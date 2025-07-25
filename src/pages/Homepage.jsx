9e8a8f211233d3abc7cd60db17f0a8422829d970
AhmedHesham <ahmed.hesham303303@gmail.com>
2025-07-25 19:10:17 +0300

    linking with Homepage.module.css

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import styles from "./Homepage.module.css";
-
+import "../index.css";
 export default function Homepage() {
   return (
     <main className={styles.homepage}>
