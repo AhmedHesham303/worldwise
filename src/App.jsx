08d4366f946fe46602c9d96afa9bed9efb5547e2
AhmedHesham <ahmed.hesham303303@gmail.com>
2025-07-25 18:54:04 +0300

    editing navigation to work with the new added starter files

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import Product from "./pages/product";
 import Pricing from "./pages/pricing";
 import Homepage from "./pages/Homepage";
 import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./pages/AppLayout";
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Homepage />} />
-
+          <Route path="app" element={<AppLayout />}></Route>
           <Route path="product" element={<Product />} />
           <Route path="pricing" element={<Pricing />} />
           <Route path="*" element={<PageNotFound />} />
