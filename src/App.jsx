d1ebc97611fa58403c89a9256adc183bf41794ef
AhmedHesham <ahmed.hesham303303@gmail.com>
2025-07-26 18:51:10 +0300

    using useNavigation

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,11 @@ import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
 import CityList from "./components/CityList";
-import { useEffect, useState } from "react";
 import CountriesList from "./components/CountriesList";
 import City from "./components/City";
+import Form from "./components/Form";
+import { useEffect, useState } from "react";
+
 const BASE_URL = "http://localhost:9000";
 function App() {
   const [cities, setCities] = useState([]);
@@ -48,7 +50,7 @@ function App() {
               path="countries"
               element={<CountriesList cities={cities} isLoading={isLoading} />}
             ></Route>
-            <Route path="form" element={<p>form</p>}></Route>
+            <Route path="form" element={<Form />}></Route>
           </Route>
           <Route path="/login" element={<Login />}></Route>
           <Route path="product" element={<Product />} />
